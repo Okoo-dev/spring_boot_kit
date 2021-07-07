80df5093ce8315512929a62b5994f03d4c707a1d
Sun <ksg95v@gmail.com>
2021-07-07 12:56:08 +0900

    Coupon , Delivery, Order, Pay VO commit

---
+++ src/main/java/edu/bit/kit/vo/OrderVO.java
@@ -0,0 +1,22 @@
+package edu.bit.kit.vo;
+
+
+import java.util.Date;
+
+import lombok.Data;
+
+
+//ORDER_NUMBER NOT NULL NUMBER(30)    
+//USER_ID      NOT NULL VARCHAR2(100) 
+//CO_DATE               DATE          
+
+
+
+
+@Data  //lombok이 제공하는 Data 어노테이션은 getter, setter, noargs, tostring 한 번에 다 만들어준다.
+public class OrderVO {
+   private int ordNumber;
+   private String userId;
+   private Date ordDate;
+    
+}
